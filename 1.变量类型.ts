caf77562d7397d2a87f339bb7933524cd89eb342
zengzutao <springworkmail@gmail.com>
2017-03-27 17:56:06 +0800

    update

--- 1.变量类型.ts
+++ 1.变量类型.ts
@@ -1,47 +1,50 @@
-//布尔
-let isDone: boolean = false;
+1.基础类型
 
-//数字
-let decLiteral: number = 6;
+(1)：布尔值：let isDone: boolean = false;
 
-//字符串
-let strname: string = "bob";
+(2)：数字：let decLiteral: number = 6;
 
-//数组
-let list: Array<number> = [1, 2, 3];
+(3)：字符串：let name: string = "bob";
 
-//元祖 Tuple
-let x: [string, number];
+(4)：模板字符串：let name:string=`Gene`;  let sentence:string =`Hello,my name is ${name}`;
 
-//枚举
-enum Color {Red, Green, Blue};
+(5)：数组：let list:number[]=[1,2,3,4];
 
-let c: Color = Color.Green;
+(6)：数组泛型：let list:Array<number>=[1,2,3,4];
 
-//任意类型（var）
-let notSure: any = 4;
+(7)：元组 （Tuple ：元组类型允许表示一个已知元素数量和类型的数组）：let x: [string, number];
 
-let varlist: any[] = [1, true, "free"];
+(8)：枚举：enum Color {Red,Green,Blue}; let c:Color=Color.Green;
 
-//空值
+          enum Color {Red = 1, Green = 2, Blue = 4}; let colorName: string = Color[2];
+
+(9)：任意值：let notSure: any = 4; notSure.ifItExists();
+
+(10)：空值（void类型像是与any类型相反，它表示没有任何类型）
+
+1).函数：无返回值
 function warnUser(): void {
     alert("This is my warning message");
 }
 
-let unusable: void = undefined || null;
+2).变量只能为 null 或 undefined
+let unusable: void = undefined;
 
-let u: undefined = undefined;
+(11)：Null 和 Undefined let u: undefined = undefined; let n: null = null;
 
-let n: null = null;
-
-
+(12)：never 类型表示的是那些永不存在的值的类型 never类型是任何类型的子类型，也可以赋值给任何类型
 // 返回never的函数必须存在无法达到的终点
 function error(message: string): never {
     throw new Error(message);
 }
 
+(13).类型断言
+1).形式一：尖括号语法
+let someValue: any = "this is a string";
 
-type C = { a: string, b?: number }
-function f({ a, b }: C): void {
-    // ...
-}
+let strLength: number = (<string>someValue).length;
+
+2).形式二：as语法
+let someValue: any = "this is a string";
+
+let strLength: number = (someValue as string).length;
--- 3.接口.ts
+++ 3.接口.ts
@@ -1,18 +1,122 @@
-//TypeScript Interface
+1.可选属性：可选属性名字定义的后面加一个?符号
 interface SquareConfig {
   color?: string;
   width?: number;
 }
 
-function createSquare(config: SquareConfig): {color: string; area: number} {
-  let newSquare = {color: "white", area: 100};
-  if (config.color) {
-    newSquare.color = config.color;
-  }
-  if (config.width) {
-    newSquare.area = config.width * config.width;
-  }
-  return newSquare;
+2.只读属性 变量：const 属性：readonly
+interface Point {
+    readonly x: number;
+    readonly y: number;
+    [propName: string]: any; 参数数量可变
 }
 
-let mySquare = createSquare({color: "black"});
+3.函数类型 只有参数列表和返回值类型的函数
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+
+let mySearch: SearchFunc;
+mySearch = function(src, sub) {
+    let result = src.search(sub);
+    if (result == -1) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+4.可索引的类型（索引签名：字符串和数字两种）
+
+1).字符串索引签名
+interface NotOkay {
+    [index:string]:string;
+}
+
+2).数子索引签名
+interface NotOkay {
+  [index:number]:string
+}
+
+3).只读索引
+interface ReadonlyStringArray {
+    readonly [index: number]: string;
+}
+
+5.类类型 实现接口 只会检查实例而不会检查类的静态部分（比如类的构造函数）
+//检查类的构造函数
+interface ClockConstructor {
+    new (hour: number, minute: number): ClockInterface;
+}
+//检查类的实例
+interface ClockInterface {
+    tick();
+}
+//构造函数
+function createClock(ctor: ClockConstructor, hour: number, minute: number): ClockInterface {
+    return new ctor(hour, minute);
+}
+
+//实例一
+class DigitalClock implements ClockInterface {
+    constructor(h: number, m: number) { }
+    tick() {
+        console.log("beep beep");
+    }
+}
+
+//实例二
+class AnalogClock implements ClockInterface {
+    constructor(h: number, m: number) { }
+    tick() {
+        console.log("tick tock");
+    }
+}
+
+let digital = createClock(DigitalClock, 12, 17);
+let analog = createClock(AnalogClock, 7, 32);
+
+6.扩展接口 一个接口可以继承多个接口，创建出多个接口的合成接口
+interface Shape {
+    color: string;
+}
+
+interface PenStroke {
+    penWidth: number;
+}
+
+interface Square extends Shape, PenStroke {
+    sideLength: number;
+}
+
+7.混合类型
+interface Counter {
+    (start: number): string;
+    interval: number;
+    reset(): void;
+}
+
+function getCounter(): Counter {
+    let counter = <Counter>function (start: number) { };
+    counter.interval = 123;
+    counter.reset = function () { };
+    return counter;
+}
+
+8.接口继承类 继承类的成员但不包括其实现 继承到类的private和protected成员 只能被这个类或其子类所实现
+class Control {
+    private state: any;
+}
+
+interface SelectableControl extends Control {
+    select(): void;
+}
+
+class Button extends Control {
+    select() { }
+}
+
+class TextBox extends Control {
+    select() { }
+}
