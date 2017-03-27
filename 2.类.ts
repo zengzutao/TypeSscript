4ea5de46113d63e2e0c5b78d8c2500899847c9ef
zengzutao <springworkmail@gmail.com>
2017-03-27 18:32:11 +0800

    update class

--- 2.类.ts
+++ 2.类.ts
@@ -1,3 +1,15 @@
+1.类：
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+2.继承：包含构造函数的派生类必须调用 super()，它会执行基类的构造方法
 class Animal {
     name:string;
     constructor(theName: string) { this.name = theName; }
@@ -14,6 +26,158 @@ class Snake extends Animal {
     }
 }
 
+3.访问修饰符：公共，私有与受保护的修饰符
+1).默认为public
+class Animal {
+    public name: string;
+    public constructor(theName: string) { this.name = theName; }
+    public move(distanceInMeters: number) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+
+2).private 当成员被标记成private时，它就不能在声明它的类的外部访问
+class Animal {
+    private name: string;
+    constructor(theName: string) { this.name = theName; }
+}
+
+3).protected protected成员在派生类中仍然可以访问
+class Person {
+    protected name: string;
+    protected constructor(theName: string) { this.name = theName; }
+}
+
+// Employee can extend Person
+class Employee extends Person {
+    private department: string;
+
+    constructor(name: string, department: string) {
+        super(name);
+        this.department = department;
+    }
+
+    public getElevatorPitch() {
+        return `Hello, my name is ${this.name} and I work in ${this.department}.`;
+    }
+}
+
+4).readonly修饰符 只读属性必须在声明时或构造函数里被初始化
+class Octopus {
+    readonly name: string;
+    readonly numberOfLegs: number = 8;
+    constructor (theName: string) {
+        this.name = theName;
+    }
+}
+let dad = new Octopus("Man with the 8 strong legs");
+dad.name = "Man with the 3-piece suit"; // error! name is readonly.
+
+5).参数属性 参数属性通过给构造函数参数添加一个访问限定符来声明
+class Animal {
+    constructor(private name: string) { }
+    move(distanceInMeters: number) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+
+4.存取器 getters/setters来截取对对象成员的访问
+let passcode = "secret passcode";
+
+class Employee {
+    private _fullName: string;
+
+    get fullName(): string {
+        return this._fullName;
+    }
+
+    set fullName(newName: string) {
+        if (passcode && passcode == "secret passcode") {
+            this._fullName = newName;
+        }
+        else {
+            console.log("Error: Unauthorized update of employee!");
+        }
+    }
+}
+
+5.静态属性 static
+class Grid {
+    static origin = {x: 0, y: 0};
+    calculateDistanceFromOrigin(point: {x: number; y: number;}) {
+        let xDist = (point.x - Grid.origin.x);
+        let yDist = (point.y - Grid.origin.y);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
+    }
+    constructor (public scale: number) { }
+}
+
+let grid1 = new Grid(1.0);  // 1x scale
+let grid2 = new Grid(5.0);  // 5x scale
+
+console.log(grid1.calculateDistanceFromOrigin({x: 10, y: 10}));
+console.log(grid2.calculateDistanceFromOrigin({x: 10, y: 10}));
+
+6.抽象类 abstract 抽象方法必须包含 abstract关键字并且可以包含访问修饰符
+抽象类做为其它派生类的基类使用
+不同于接口，抽象类可以包含成员的实现细节
+一般不会直接被实例化
+抽象方法的语法与接口方法相似，两者都是定义方法签名但不包含方法体
+abstract class Animal {
+    abstract makeSound(): void;
+    move(): void {
+        console.log('roaming the earch...');
+    }
+}
+
+1).抽象类中的抽象方法不包含具体实现并且必须在派生类中实现
+abstract class Department {
+
+    constructor(public name: string) {}
+
+    printName(): void {
+        console.log('Department name: ' + this.name);
+    }
+
+    abstract printMeeting(): void; // 必须在派生类中实现
+}
+
+class AccountingDepartment extends Department {
+
+    constructor() {
+        super('Accounting and Auditing'); // constructors in derived classes must call super()
+    }
+
+    printMeeting(): void {
+        console.log('The Accounting Department meets each Monday at 10am.');
+    }
+
+    generateReports(): void {
+        console.log('Generating accounting reports...');
+    }
+}
+
+7.高级技巧
+1).构造函数 构造函数也包含了类的所有静态属性
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+let greeter: Greeter;
+greeter = new Greeter("world");
+console.log(greeter.greet());
+
+let greeter;
+greeter = new Greeter("world");
+console.log(greeter.greet());
+
+2.把类当做接口使用
 class Point {
     x: number;
     y: number;
@@ -24,9 +188,3 @@ interface Point3d extends Point {
 }
 
 let point3d: Point3d = {x: 1, y: 2, z: 3};
-
-let myAdd1: (x:number, y:number) => number =
-    function(x: number, y: number): number { return x+y; };
-
-let myAdd2: (baseValue:number, increment:number) => number =
-    function(x: number, y: number): number { return x + y; };
